aa7e56d2623bb0497186e7c39211e319bf2f7507
pettijohn <pettijohn@gmail.com>
2017-06-30 20:25:02 -0700

    Cleanup

--- program.py
+++ program.py
@@ -23,25 +23,15 @@ if not p.exists():
 
     alltxns = []
 
-    print('account, txid, created_at, type, crypto_amount, crypto_currency, fiat_amount, fiat_currency')
-
     for account in accounts.data:
-        # print(account.id)
         txns = client.get_transactions(account.id, limit=25)
-        for tx in txns.data:
-            alltxns.append(tx)
+        alltxns += txns.data
         while True: 
-            
-            for tx in txns.data:
-                print(account.id + ', ' + tx.id + ', ' + tx.created_at + ', ' + tx.type + ', ' + 
-                    tx.amount.amount + ', ' + tx.amount.currency + ', ' + tx.native_amount.amount + ', ' + tx.native_amount.currency)
-
             if txns.pagination.next_uri != None:
                 # next_uri='/v2/accounts/7e421ebf-9b0f-5b40-b0ef-c4d82c9d943e/transactions?starting_after=3a42e194-ef17-50a6-bafe-c946c0b7dfd5'
                 starting_after_guid = re.search('starting_after=([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})', txns.pagination.next_uri, re.I)[1]
                 txns = client.get_transactions(account.id, limit=25, starting_after=starting_after_guid)
-                for tx in txns.data:
-                    alltxns.append(tx)
+                alltxns += txns.data
             else:
                 break
 
